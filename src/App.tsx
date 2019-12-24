b132ef4b848acb7b3cc735175052131194501a3d
Sebastp desktop app <sebastianpob@gmail.com>
2019-12-24 17:01:58 +0100

    change App.tsx path/app entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,32 @@
-import * as React from 'react'
-import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import React from 'react'
+import { registerRootComponent } from 'expo'
+import { StyleSheet, Text, View } from 'react-native'
+import { ApolloProvider } from '@apollo/react-hooks'
 
-import { AppLoading } from 'expo'
+//HELPERS
+// import apollo from '~lib/apolloClient'
+import apollo from './lib/apolloClient'
 
-import { Asset } from 'expo-asset'
-import * as Font from 'expo-font'
-
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloClient } from 'apollo-client'
-// import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from 'react-apollo'
-
-import { mockedLink } from './mock'
-import AppNavigator from './navigation/AppNavigator'
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: mockedLink, // new HttpLink()
-})
+function App() {
+  return (
+    <ApolloProvider client={apollo}>
+      <View style={styles.container}>
+        <Text style={styles.text}>To możesz</Text>
+      </View>
+    </ApolloProvider>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
     flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 })
 
-interface Props {
-  skipLoadingScreen: boolean
-}
-
-interface States {
-  isLoadingComplete: boolean
-}
-
-export default class App extends React.Component<Props, States> {
-  public state = {
-    isLoadingComplete: false,
-  }
-
-  private loadResourcesAsync = async () => {
-    await Promise.all([
-      Asset.loadAsync([
-        // ...
-      ]),
-      Font.loadAsync({
-        // ...
-      }),
-    ])
-  }
-
-  private handleLoadingError = () => {
-    // ...
-  }
-
-  private handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true })
-  }
-
-  public render() {
-    const { isLoadingComplete } = this.state
-    const { skipLoadingScreen } = this.props
-    if (!isLoadingComplete && !skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this.loadResourcesAsync}
-          onError={this.handleLoadingError}
-          onFinish={this.handleFinishLoading}
-        />
-      )
-    }
-    return (
-      <ApolloProvider client={client}>
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      </ApolloProvider>
-    )
-  }
-}
+export default registerRootComponent(App)
